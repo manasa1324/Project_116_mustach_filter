28df40ff67dee5dac4f8467a379801b1afa68b9f
manasa1324 <79976512+manasa1324@users.noreply.github.com>
2021-09-29 22:28:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+function preload()
+{
+    mustach = loadImage("Mustach_filter.png")
+    clown_nose = loadImage("https://i.postimg.cc/q7Rpk1HC/Clown-nose-1png-removebg-preview-1.png");
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x-20;
+        noseY = results[0].pose.nose.y-2;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    image(mustach, noseX, noseY, 40, 30);
+}
+
+function take_snapshot()
+{
+    save('myPicture.png');
+}
